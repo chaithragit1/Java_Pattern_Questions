0a18f3104b99d101318085c67701b98f52d97cfc
chaithra <90747040+chaithragit1@users.noreply.github.com>
2024-03-13 22:55:06 +0530

    Update Pattern24.java

--- JavaPattern/Pattern24.java
+++ JavaPattern/Pattern24.java
@@ -1,43 +1,52 @@
-package JavaPattern;
+package Javapattern;
 
 public class Pattern24 {
 
 	public static void main(String[] args) {
-		int n=5;
-		for(int i=1;i<=n;i++)
+		for(int i=1;i<=5;i++)
 		{
-				for(int k=1;k<=5-i+1;k++)
+			for(int k=1;k<=5-i+1;k++)
 			{
-				System.out.print("-");
+				System.out.print(" "+" ");
 			}
-				
-				for(int j=1;j<=2*i-1;j++)
+			for(int j=1;j<=2*i-1;j++)
+			{
+				if(j==1)
+				System.out.print("1"+ " ");
+				else if(j==3)
 				{
-				if(j==1||j==5)
+					System.out.print(i-1+" ");
+				}
+				else if(j==5)
 				{
-					System.out.println(1);
+					if(i==3)
+						System.out.print(i-2+" ");
+					if(i==4)
+						   System.out.print(i-1+" ");
+					  if(i==5)
+						  System.out.print(i+1+" ");
+				}
+				else if(j==7)
+				{
+				 if(i==4)
+					System.out.print(1+" ");
+				 else
+				   System.out.print(i-1+" ");
+
+				}
+				else if(j==9)
+				{
+				    
+					System.out.print(i-4+" ");
 				}
 				else
 				{
-					System.out.println(i);
+					System.out.print(" "+" ");
 				}
-				for(int k=1;k<=i;k++)
-				{
-					if(k==0||k==1||k==2||k==3||k==4)
-					{
-					System.out.print("$");
-					}
-					
-				}
-		
-		
-}
+			}
+			System.out.println();
 		}
-		
-		
+
 	}
+
 }
-		
-
-
-
